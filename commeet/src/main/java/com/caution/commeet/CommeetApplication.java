2e17c40ab83c3076bc09dc8406919068ef6d2d8b
Park Hoyoung <hopremium5123@gmail.com>
2025-09-24 19:27:23 +0900

    commeet initial

---
+++ commeet/src/main/java/com/caution/commeet/CommeetApplication.java
@@ -0,0 +1,13 @@
+package com.caution.commeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommeetApplication.class, args);
+	}
+
+}
---
+++ commeet/src/test/java/com/caution/commeet/CommeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caution.commeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
